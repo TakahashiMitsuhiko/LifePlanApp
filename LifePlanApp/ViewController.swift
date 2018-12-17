6ce6aa00aa0fa065b21aa83f680d45717ab84e8c
ICTクルー <ict_crew@meguminoimac.local>
2018-12-17 14:50:11 +0900

    Initial Commit

---
+++ LifePlanApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifePlanApp
+//
+//  Created by ICTクルー on 2018/12/17.
+//  Copyright © 2018年 ICTクルー. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

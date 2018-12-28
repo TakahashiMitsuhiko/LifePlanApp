96ffa5dc5bf881ffa5dcbcd77fe9a95d4f21876e
toy101 <45931528+toy101@users.noreply.github.com>
2018-12-28 16:08:12 +0900

    プログラムからボタンの設定
    はじめるのボタンの設定

--- LifePlanApp/ViewController.swift
+++ LifePlanApp/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Start: UIButton!//「はじめる」のボタン
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //「はじめる」のボタンの設定
+        Start.layer.borderColor = UIColor.blue.cgColor
+        Start.layer.borderWidth = 1.0
+        Start.layer.cornerRadius = 10.0
     }
 
 

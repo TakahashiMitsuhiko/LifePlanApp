17fa1cb5b0f09808f57fef88f1816aa4f5cc82df
toy101 <45931528+toy101@users.noreply.github.com>
2019-01-07 01:08:55 +0900

    ボタンの設定
    背景透明
    枠あり丸角
    
    複数のボタンを一括で設定する方法はないか？

--- LifePlanApp/ViewController.swift
+++ LifePlanApp/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+	
+	@IBOutlet weak var StartButton: UIButton!
+	
+	
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		
+		StartButton.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0)
+		let StartButtonColor = UIColor(red: 0/255, green: 172/255, blue: 237/255, alpha: 100)
+		StartButton.layer.borderWidth = 1.5
+		StartButton.layer.borderColor = StartButtonColor.cgColor
+		StartButton.layer.cornerRadius = 5
+		StartButton.setTitleColor(StartButtonColor, for: UIControl.State.normal)
+		
     }
 
 

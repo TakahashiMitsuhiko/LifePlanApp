c505ac426af68c9f3c42ccc85a3ad6dece4986fd
TakahashiMitsuhiko <46039719+TakahashiMitsuhiko@users.noreply.github.com>
2019-01-07 16:01:25 +0900

    明けましておめでとうございます

--- LifePlanApp/ViewController.swift
+++ LifePlanApp/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+            func ButtonTouchDown(_ sender: Any) {
+            let url=NSURL(string:"http://www.apple.com")
+            if UIApplication.shared.canOpenURL(url! as URL){
+                UIApplication.shared.open(url! as URL,options: [:],completionHandler: nil)
+            }
+            //let request=NSURLRequest(url:NSURL(string:"http://www.apple.com")! as URL);UIWebView.loadRequest(request)
+        }
+        
     }
 
 
